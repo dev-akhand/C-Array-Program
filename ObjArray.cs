ff3f0e995a529d8c2a111beb951708536536368e
Akhand Pratap Singh <113235039+dev-akhand@users.noreply.github.com>
2023-12-09 20:51:49 +0530

    Add files via upload

---
+++ ObjArray.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array = new object[3];
+            array[0] = 1;
+            array[1] = "Akhand";
+            Customer c = new Customer();
+            c.ID = 99;
+            c.Name = "Singh";
+            array[2] = c;
+            foreach (object  item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
